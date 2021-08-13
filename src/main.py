7eaa78928f9adfb54f1deef2caf8b1b841c8f15a
NefeliTav <msstama2@yahoo.gr>
2021-08-13 13:25:02 +0300

    compare results

--- src/main.py
+++ src/main.py
@@ -7,18 +7,18 @@ from MLP_model import train_MLP
 
 if __name__ == "__main__":
     x_train, x_test, y_train, y_test, scaler, price, lookback = preprocess()
-    '''
-    lstm = train_LSTM(x_train, x_test, y_train, y_test,
-                      y_train_gru, y_test_gru, scaler, price, lookback)
-    gru = train_GRU(x_train, x_test, y_train, y_test,
-                    y_train_gru, y_test_gru, scaler, price, lookback)
+
+    lstm = train_LSTM(x_train, x_test, y_train,
+                      y_test, scaler, price, lookback)
+    gru = train_GRU(x_train, x_test, y_train, y_test, scaler, price, lookback)
+
+    mlp = train_MLP(x_train, x_test, y_train, y_test, scaler, price, lookback)
 
     lstm = pd.DataFrame(lstm, columns=['LSTM'])
     gru = pd.DataFrame(gru, columns=['GRU'])
-    result = pd.concat([lstm, gru], axis=1, join='inner')
+    mlp = pd.DataFrame(mlp, columns=['MLP'])
+    result = pd.concat([lstm, gru, mlp], axis=1, join='inner')
     result.index = ['Train RMSE', 'Test RMSE', 'Train Time']
     print()
     print(result)
     print()
-    '''
-    mlp = train_MLP(x_train, x_test, y_train, y_test, scaler, price, lookback)
