30d5d6c56d660ae615d8b531eb34f05f3ee3d37a
NefeliTav <msstama2@yahoo.gr>
2021-08-19 00:10:48 +0300

    update report

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ from CNN_model import train_CNN
 if __name__ == "__main__":
     x_train, x_test, y_train, y_test, scaler, price, lookback = preprocess()
 
+    mlp = train_MLP(x_train, x_test, y_train, y_test, scaler, price, lookback)
+
     cnn = train_CNN(x_train, x_test, y_train,
                     y_test, scaler, price, lookback)
 
@@ -16,14 +18,12 @@ if __name__ == "__main__":
                       y_test, scaler, price, lookback)
     gru = train_GRU(x_train, x_test, y_train, y_test, scaler, price, lookback)
 
-    mlp = train_MLP(x_train, x_test, y_train, y_test, scaler, price, lookback)
-
     lstm = pd.DataFrame(lstm, columns=['LSTM'])
     gru = pd.DataFrame(gru, columns=['GRU'])
     mlp = pd.DataFrame(mlp, columns=['MLP'])
     cnn = pd.DataFrame(cnn, columns=['CNN'])
 
-    result = pd.concat([lstm, gru, mlp], axis=1, join='inner')
+    result = pd.concat([mlp, cnn, lstm, gru], axis=1, join='inner')
     result.index = ['Train RMSE', 'Test RMSE', 'Train Time']
     print()
     print(result)
